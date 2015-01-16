b8450ad84232fd5a9af7d449d03793431f3ef7f7
Oles Kashchenko <oleskashchenko@gmail.com>
2015-01-16 15:13:07 +0200

    first

---
+++ src/OlesKashchenko/AlfabankApi/AlfabankApiServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace OlesKashchenko\AlfabankApi;
+
+use Illuminate\Support\ServiceProvider;
+
+class AlfabankApiServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('oles-kashchenko/alfabank-api');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
